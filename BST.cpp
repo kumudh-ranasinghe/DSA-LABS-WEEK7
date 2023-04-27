805eff3e186ae94dab19ff2f9c4fb46336521a89
kumudh-ranasinghe <127022266+kumudh-ranasinghe@users.noreply.github.com>
2023-04-27 14:33:25 +0530

    Add files via upload

---
+++ BST.cpp
@@ -0,0 +1,129 @@
+#include <iostream>
+using namespace std;
+
+struct node {
+  int key;
+  struct node *left, *right;
+};
+
+// Inorder traversal
+void traverseInOrder(struct node *root) {
+  if (root != NULL) {
+    traverseInOrder(root->left);
+    cout << root->key << " ";
+    traverseInOrder(root->right);
+  }
+}
+
+// Insert a node
+struct node *insertNode(struct node *root, int key) {
+  if (root == NULL) {
+    struct node *newNode = new node;
+    newNode->key = key;
+    newNode->left = newNode->right = NULL;
+    return newNode;
+  }
+  //nodes with existing values are not inserted because it is redundant
+  if (key < root->key)
+    root->left = insertNode(root->left, key);
+  else if (key > root->key)
+    root->right = insertNode(root->right, key);
+
+  return root;
+}
+
+// Find minimum node in the subtree rooted at given node
+struct node *minNode(struct node *node) {
+  struct node *current = node;
+
+  while (current && current->left != NULL)
+    current = current->left;
+
+  return current;
+}
+
+// Deleting a node
+struct node *deleteNode(struct node *root, int key) {
+  if (root == NULL)
+    return root;
+
+  if (key < root->key)
+    root->left = deleteNode(root->left, key);
+  else if (key > root->key)
+    root->right = deleteNode(root->right, key);
+  else {
+    if (root->left == NULL) {
+      struct node *temp = root->right;
+      delete root;
+      return temp;
+    }
+    else if (root->right == NULL) {
+      struct node *temp = root->left;
+      delete root;
+      return temp;
+    }
+
+    struct node *temp = minNode(root->right);
+    root->key = temp->key;
+    root->right = deleteNode(root->right, temp->key);
+  }
+
+  return root;
+}
+
+// Driver code
+int main() {
+  struct node *root = NULL;
+
+  int operation;
+  int operand;
+  cin >> operation;
+
+  while (operation != -1) {
+    switch(operation) {
+      case 1: // insert
+        cin >> operand;
+        root = insertNode(root, operand);
+        cin >> operation;
+        break;
+      case 2: // delete
+        cin >> operand;
+        root = deleteNode(root, operand);
+        cin >> operation;
+        break;
+      default:
+        cout << "Invalid Operator!\n";
+        return 0;
+    }
+  }
+  
+  traverseInOrder(root);
+  return 0;
+}
+  struct node *root = NULL;
+
+  int operation;
+  int operand;
+  cin >> operation;
+
+  while (operation != -1) {
+    switch(operation) {
+      case 1: // insert
+        cin >> operand;
+        root = insertNode(root, operand);
+        cin >> operation;
+        break;
+      case 2: // delete
+        cin >> operand;
+        root = deleteNode(root, operand);
+        cin >> operation;
+        break;
+      default:
+        cout << "Invalid Operator!\n";
+        return 0;
+    }
+  }
+  
+  traverseInOrder(root);
+  return 0;
+}
